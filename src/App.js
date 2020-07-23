6c0fff1651a8e1613a1c1b0a678002712cd9b26f
Gerardo Arjona <gerardo.arj15@gmail.com>
2020-07-22 19:01:07 -0500

    add layout with footer and nabar
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Layout from '../components/layout';
+import Layout from './components/layout';
 
 function App() {
   return (
