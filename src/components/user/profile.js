7d1052a1e296f37a20751c2eee3b389c31bb3515
Gerardo Arjona <gerardo.arj15@gmail.com>
2020-07-23 22:43:30 -0500

    remove hisotry from profile
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import { useHistory } from 'react-router-dom';
-import { getUserTok, isLoggedIn } from '../../services/auth'
+import { getUserTok } from '../../services/auth'
 
 // import ReactNotification, {store} from 'react-notifications-component'
 // import 'react-notifications-component/dist/theme.css'
@@ -9,34 +8,28 @@ import { getUserTok, isLoggedIn } from '../../services/auth'
 import '../../App.css';
 
 function Profile(props) {
-
-    //let history = useHistory();
     const [user, setUser] = useState([]);
 
     useEffect(() => {
-        // if(isLoggedIn()){
-            let tok = getUserTok()
-            const header = {
-                headers:{
-                    Authorization: `Bearer ${tok}` 
-                }
+        let tok = getUserTok()
+        const header = {
+            headers:{
+                Authorization: `Bearer ${tok}` 
             }
-            const { id } = props.match.params
-            const fetchData = async () => {
-                axios.get(
-                    `${process.env.REACT_APP_API_URL}/users/${id}`, header
-                ).then( result =>{
-                        console.log(result.data)
-                        setUser(result.data)
-                    }
-                ).catch(e => {
-                    setUser({})
-                })
-            };
-            fetchData();
-        // }else{
-        //     history.push(`/users`)
-        // }
+        }
+        const { id } = props.match.params
+        const fetchData = async () => {
+            axios.get(
+                `${process.env.REACT_APP_API_URL}/users/${id}`, header
+            ).then( result =>{
+                    console.log(result.data)
+                    setUser(result.data)
+                }
+            ).catch(e => {
+                setUser({})
+            })
+        };
+        fetchData();
 	}, []);
 
     return (
